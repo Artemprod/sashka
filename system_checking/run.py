2212d7386f41a3d2ce20a52c21f85cd77178c350
artemprod <artemprod1@gmail.com>
2024-11-10 13:47:48 +0400

    fix postgress data path

--- system_checking/run.py
+++ system_checking/run.py
@@ -6,7 +6,7 @@ from system_checking.nats_chekcer import NatsChecker
 
 
 async def streams_checking():
-    streams: list[FirstMessage | MessageSend] = [nats_distributor_settings.message.first_message_message, nats_distributor_settings.message.send_message]
+    streams = [nats_distributor_settings.message.first_message_message, nats_distributor_settings.message.send_message]
     url = nast_base_settings.nats_server_url
     nats_checker = NatsChecker(nats_url=url, nats_streams=streams)
     await nats_checker.check()
