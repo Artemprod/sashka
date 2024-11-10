ff7a3df146c6dc7cb7a64db2bba340fd778637d9
Artem Portnov <57044749+Artemprod@users.noreply.github.com>
2024-11-10 14:40:12 +0400

    Update run.py

--- system_checking/run.py
+++ system_checking/run.py
@@ -8,6 +8,7 @@ from system_checking.nats_chekcer import NatsChecker
 async def streams_checking():
     streams = [nats_distributor_settings.message.first_message_message, nats_distributor_settings.message.send_message]
     url = nast_base_settings.nats_server_url
+    print(url)
     nats_checker = NatsChecker(nats_url=url, nats_streams=streams)
     await nats_checker.check()
 
