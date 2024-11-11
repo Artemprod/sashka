d1fd7dc748804c0cb63bc738135be78b97326d9a
artemprod <artemprod1@gmail.com>
2024-11-11 11:39:50 +0400

    fix dto user

--- system_checking/run.py
+++ system_checking/run.py
@@ -7,10 +7,9 @@ from system_checking.nats_chekcer import NatsChecker
 
 async def streams_checking():
     streams = [nats_distributor_settings.message.first_message_message, nats_distributor_settings.message.send_message]
-    # url = nast_base_settings.nats_server_url
-    url = "nats://194.87.79.10:4000"
+    url = nast_base_settings.nats_server_url
     nats_checker = NatsChecker(nats_url=url, nats_streams=streams)
-    # await nats_checker.check()
-    await nats_checker.check_connection()
+    await nats_checker.check()
+    # await nats_checker.check_connection()
 asyncio.run(streams_checking())
 
