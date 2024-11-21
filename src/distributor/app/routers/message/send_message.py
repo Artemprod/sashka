a55eb78519e77d17ddc82b64bd29ca642c587a20
https://github.com/Artemprod/ai_request_api <artemprod1@gmail.com>
2024-11-21 13:04:04 +0400

    fix message bugs

--- src/distributor/app/routers/message/send_message.py
+++ src/distributor/app/routers/message/send_message.py
@@ -8,7 +8,7 @@ from nats.js.api import DeliverPolicy
 from nats.js.api import RetentionPolicy
 
 from configs.nats_queues import nats_distributor_settings
-from src.distributor.app.dependency.message import _get_data_from_headers
+from src.distributor.app.dependency.message import _get_data_from_headers, get_telegram_client, get_data_from_body
 from src.distributor.app.schemas.message import Datas
 from src.distributor.app.utils.message import send_message
 
@@ -30,9 +30,9 @@ async def send_first_message_subscriber(body: str, msg: NatsMessage, context=Con
         logger.info(f"Message {data.user, body}  delayed for {delay} ")
     else:
         try:
-            msg_data = await send_message(data.client, data.user, body)
-            logger.info(f"Message sent at {data.current_time}: {msg_data}")
             await msg.ack()
+            msg_data = await send_message(data.client, data.user, message=body)
+            logger.info(f"Message sent at {data.current_time}: {msg_data}")
         except Exception as e:
             logger.error(f"Error while sending message: {e}")
             raise e
@@ -43,14 +43,15 @@ async def send_first_message_subscriber(body: str, msg: NatsMessage, context=Con
                    subject=nats_distributor_settings.message.send_message.subject,
                    deliver_policy=DeliverPolicy.ALL,
                    no_ack=True)
-async def send_message_subscriber(body: str, msg: NatsMessage, context=Context(), data=Depends(_get_data_from_headers)):
+async def send_message_subscriber(body: str, msg: NatsMessage, context=Context(), data=Depends(get_data_from_body), client=Depends(get_telegram_client)):
     """Send a conversation message."""
-    logger.info("Sending message...")
-
+    await msg.ack()
+    logger.warning("message acked")
     try:
-        msg_data = await send_message(data.client, data.user, body)
+        logger.info("Sending message...")
+        msg_data = await send_message(client=client,user=data.user,message=data.message)
         logger.info(f"Message sent: {msg_data}")
-        await msg.ack()
+
     except Exception as e:
         logger.error(f"Failed to send message: {e}")
         raise e
