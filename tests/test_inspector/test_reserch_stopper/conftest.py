2b3b73ed8ab22dbe015e7ff8fe0809571b2d503d
https://github.com/Artemprod/ai_request_api <artemprod1@gmail.com>
2024-12-17 12:24:59 +0400

    add global conftest

--- tests/test_inspector/test_reserch_stopper/conftest.py
+++ tests/test_inspector/test_reserch_stopper/conftest.py
@@ -4,11 +4,11 @@ from src.database.repository.storage import RepoStorage
 from src.services.research.telegram.inspector import ResearchOverChecker, ResearchStopper
 
 
-@pytest.fixture
+@pytest.fixture(scope="module", autouse=True)
 def load_repository(mocker):
     return mocker.Mock(spec=RepoStorage)
 
-@pytest.fixture
+@pytest.fixture(scope="module", autouse=True)
 def load_research_stopper(load_repository)->ResearchStopper:
     return ResearchStopper(repository=load_repository)
 
--- tests/test_inspector/test_reserch_user_in_done_stopper/conftest.py
+++ tests/test_inspector/test_reserch_user_in_done_stopper/conftest.py
@@ -4,15 +4,15 @@ from src.database.repository.storage import RepoStorage
 from src.services.research.telegram.inspector import  ResearchStopper, UserDoneStopper
 
 
-@pytest.fixture
+@pytest.fixture(scope="module", autouse=True)
 def load_repository(mocker):
     return mocker.Mock(spec=RepoStorage)
 
-@pytest.fixture
+@pytest.fixture(scope="module", autouse=True)
 def load_stopper(mocker):
     return mocker.Mock(spec=ResearchStopper)
 
-@pytest.fixture
+@pytest.fixture(scope="module", autouse=True)
 def load_user_done(load_repository,load_stopper)->UserDoneStopper:
     instance = UserDoneStopper(repository=load_repository,stopper=load_stopper)
     instance.settings['delay_check_interval'] = 0.1
