41ef53fd71179ce22ab3acc179166d1707a6be10
https://github.com/Artemprod/ai_request_api <artemprod1@gmail.com>
2025-01-28 12:22:34 +0400

    fix test files

--- tests/test_communicator/conftest.py
+++
@@ -1,8 +0,0 @@
-# import pytest
-#
-# from src.dispatcher.communicators.reggestry import ConsoleCommunicator
-#
-#
-# @pytest.fixture(scope="package")
-# def get_console_communicator():
-#     return ConsoleCommunicator()
